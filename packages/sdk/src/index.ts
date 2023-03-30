c74460ebc4e5a3e39a845b5f05959f681c9aa6ab
Allan Tong <atong@systemautomation.com>
2023-03-30 09:45:40 -0400

    chore: configure file headers

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,2 +1,5 @@
+// Copyright (c) 2023 System Automation Corporation.
+// This file is licensed under the MIT License.
+
 export * from '@evoke-platform/context';
 export * from '@evoke-platform/ui-components';
