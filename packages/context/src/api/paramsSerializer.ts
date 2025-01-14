537517669fb3bc28c9c1b458c427b79c2620f971
Minh Truong <mtruong@systemautomation.com>
2025-01-14 14:00:17 -0500

    fix: filter out undefined params

--- packages/context/src/api/paramsSerializer.ts
+++ packages/context/src/api/paramsSerializer.ts
@@ -1,11 +1,24 @@
 import { ParamsSerializerOptions } from 'axios';
+import _ from 'lodash';
 
 export function paramsSerializer(params: Record<string, unknown>, options?: ParamsSerializerOptions) {
     const searchParams = new URLSearchParams();
 
     for (const [key, value] of Object.entries(params)) {
+        if (!validateParamKey(key) || !validateParamValue(value)) {
+            continue;
+        }
+
         searchParams.append(key, typeof value !== 'string' ? JSON.stringify(value) : value);
     }
 
     return searchParams.toString();
+}
+
+function validateParamKey(item?: string) {
+    return !_.isNil(item) && !_.isUndefined(item) && !(_.isString(item) && _.isEmpty(item));
+}
+
+function validateParamValue(item?: unknown) {
+    return !_.isUndefined(item);
 }
