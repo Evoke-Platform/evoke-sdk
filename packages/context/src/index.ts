f4dc67597053647ce79eb05ebcd9221ec79343e5
Brad Wray <bradwray@gmail.com>
2025-03-28 15:42:01 -0400

    refactor: remove bespoke options, update tests

--- packages/context/src/index.ts
+++ packages/context/src/index.ts
@@ -6,6 +6,5 @@ export * from './app/index.js';
 export * from './authentication/index.js';
 export * from './notification/index.js';
 export * from './objects/index.js';
-export * from './objects/utils.js';
 export * from './router/index.js';
 export * from './signalr/index.js';
