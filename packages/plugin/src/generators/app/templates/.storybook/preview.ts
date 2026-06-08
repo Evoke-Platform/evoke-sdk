9b3490723df6b2f401d92ec11200c7b465facb63
Minh Truong <mtruong@systemautomation.com>
2026-06-08 14:59:22 -0400

    chore(plugin/CDR-2943): add default theme to storybook

--- packages/plugin/src/generators/app/templates/.storybook/preview.ts
+++ packages/plugin/src/generators/app/templates/.storybook/preview.ts
@@ -1,6 +1,11 @@
+import { UIThemeProvider, defaultTheme } from '@evoke-platform/ui-components';
 import type { Preview } from '@storybook/react';
+import React from 'react';
+
+const withTheme = (Story) => React.createElement(UIThemeProvider, { theme: defaultTheme }, React.createElement(Story));
 
 const preview: Preview = {
+    decorators: [withTheme],
     parameters: {
         actions: { argTypesRegex: '^on[A-Z].*' },
         controls: {
